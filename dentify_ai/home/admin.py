c469c8be59dd338cbb5211fac6935ca8731c9a39
mustafaqasimali <116185630+mustafaqasimali@users.noreply.github.com>
2024-10-19 00:35:18 +0530

    updating contact_form backend with client side email

--- dentify_ai/home/admin.py
+++ dentify_ai/home/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from home.models import Prediction
+from home.models import ContactMessage
 
 admin.site.register(Prediction)
+admin.site.register(ContactMessage)
+
